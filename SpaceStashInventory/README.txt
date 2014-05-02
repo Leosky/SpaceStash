854e4c7d57d41a53acd6889b6c5c244c8eaa7925
Leosky <kheless@gmail.com>
2014-05-03 01:14:42 +0200

    beta2

--- SpaceStashInventory/README.txt
+++ SpaceStashInventory/README.txt
@@ -1,7 +1,12 @@
 Overview
 ========
 This addon aim to be an Wildstar equivalent of Bagnon. You can configure this size and basic appearance to have a light replacement of the huge Inventory stock addon.
-Currently the addon don't remove the stock Inventory addon to let you access virtual inventory and crafting inventory until this feature is added to SpaceStashInventory. For quick access, you can create a macro to bind the addon to a key on your bar with "/ssi" command.
+
+New in this version
+===================
+* Keybinding ; no need to make this crappy macro anymore !
+* Previewin', splitin', deletin', salvagin' n 'deletin' (drag and drop out of the inventory) YEEEEEAH boy!
+* New style more in phase with base UI and some sounds, cuz previous hurted some artistic sens... Damn, it mean i sill don't have the artistic sens, I should drink more whiskey.
 
 Commands
 ========
@@ -10,39 +15,44 @@ Commands
 /ssi option RowSize [number] : define the number of item per row you want.
 /ssi option IconSize [number] : define size of item icons.
 /ssi option currency [ElderGems,Prestige,Renown,CraftingVouchers] : define the currently tracked alternative currency.
-/ssi redraw : debuging purpose - redraw the bag window
-/ssi info : debuging purpose - send the metatable to GeminiConsole
+/ssi redraw : debuging purpose  redraw the bag window
+/ssi info : debuging purpose send the metatable to console
 
 Missing features before 1.0
 ===========================
-The addon is in an early stage so some features are missing. Here is a non-exhaustive list :
+The addon is in an early stage so some features are missing. Here is a nonexhaustive list :
 
 1	Config : A window to configure the addon without commands
 2.	Bags : Add a toggle button to display or not bags item.
 3.	Currencies : Add two small button to change the alternative currency displayed and/or a little menu on right clic.
 4.	Bank : Change the stock bank addon with SpaceStashBank.
 5.	Search box : highlight item in your inventory based on search patterns
-6.	Salvaging : Chose between the current ImprovedSalvage or a toggle button highlight salvageable items and to salvage clicked items. 
 
 Possible features
 =================
 This features mostly depend of evolution of addon Libraries like Gemini.
 
-*	Ability to other modder to define the salvaging and previewing method
+*	Ability to other modder to define the salvaging and previewing methods.
 *	Config profiles
 *	Alt bag access to view items in your alt inventory / bank
-*	A customizable data display of other addon
+*	A customizable data display of other addon.
+*	Bag sorting.
+
 
 Known bugs
 ==========
-*	Inventory dont resize with bag changes.
-*	The new item sprit dont fade until the item is moved.
-*	Impossible to destroy items.
+The new item sprit dont fade until the item is moved or the inventory closed.
 
 Changelog
 =========
-beta2
+beta2 
 -----
+*	The addon now remplace the base addon without need to rebind the inventory keybinding or use a macro.
+*	Addon added to the menu list.
+*	Item now are marked as viewed when then inventory is closed or moved (not on mouse over actually)
+*	Salvaging button open a Salvaging window.
+*	Added location and currency saving between characters.
+*	Addded item preview and split.
 *	Added an icon for item that cannot be used.
 *	Removed GeminiConsole dependancy
 *	Added a command /ssi redraw for debuging purpose.
@@ -50,6 +60,6 @@ beta2
 *	Fixed command /ssi option currency.
 *	Command /ssi option BoxPerRow and SquarePerRow have been renamed RowSize and IconSize.
 
-beta1
+beta1 
 -----
-*	Initial release
+Initial release
--- SpaceStashInventory/SpaceStashInventory.lua
+++ SpaceStashInventory/SpaceStashInventory.lua
@@ -8,6 +8,7 @@ require "GameLib"
 require "Item"
 require "Window"
 require "Money"
+require "Sound"
 
 -----------------------------------------------------------------------------------------------
 -- SpaceStashInventory Module Definition
@@ -26,40 +27,37 @@ local Keybind
 -----------------------------------------------------------------------------------------------
 -- Constants
 -----------------------------------------------------------------------------------------------
-local MAJOR, MINOR = "SpaceStashInventory-Beta", 1
+local MAJOR, MINOR = "SpaceStashInventory-Beta", 2
 
 local kcrSelectedText = ApolloColor.new("UI_BtnTextHoloPressedFlyby")
 local kcrNormalText = ApolloColor.new("UI_BtnTextHoloNormal")
 local tCurrencies = {}
-tCurrencies["eldergems"] = Money.CodeEnumCurrencyType.ElderGems
-tCurrencies["prestige"] = Money.CodeEnumCurrencyType.Prestige
-tCurrencies["renown"] = Money.CodeEnumCurrencyType.Renown
-tCurrencies["craftingvouchers"] = Money.CodeEnumCurrencyType.CraftingVouchers
+tCurrencies["ElderGems"] = Money.CodeEnumCurrencyType.ElderGems
+tCurrencies["Prestige"] = Money.CodeEnumCurrencyType.Prestige
+tCurrencies["Renown"] = Money.CodeEnumCurrencyType.Renown
+tCurrencies["CraftingVouchers"] = Money.CodeEnumCurrencyType.CraftingVouchers
 
 
 function SpaceStashInventory:OnLoad()
 	inspect = Apollo.GetPackage("Drafto:Lib:inspect-1.2").tPackage
 	GeminiLogging = Apollo.GetPackage("Gemini:Logging-1.2").tPackage
-	GeminiConsole = Apollo.GetAddon("GeminiConsole")	
-	ImprovedSalvage = Apollo.GetAddon("ImprovedSalvage")
 
 	self.glog = GeminiLogging:GetLogger({
-		  level = GeminiLogging.INFO,
+		  level = GeminiLogging.DEBUG,
 		  pattern = "%d [%c:%n] %l - %m",
-		  appender = "GeminiConsole"
+		  appender = "Print"
 		})
-	
-	
 
 	self.tConfig = {}
 	self.tConfig.version = {}
 	self.tConfig.version.MAJOR = MAJOR
 	self.tConfig.version.MINOR = MINOR
-	self.tConfig.window = { SquareSize = 48, BoxPerRow = 10}
+	self.tConfig.window = { IconSize = 48, RowSize = 10}
 	self.tConfig.window.location = { fPoints = {0,0,0,0}, nOffsets = {64,64,576,756}}
 	self.tConfig.currencies = {eCurrencyType = Money.CodeEnumCurrencyType.Renown}
 
 	self.nInventorySize = 16
+
     -- load our form file
 	self.xmlDoc = XmlDoc.CreateFromFile("SpaceStashInventory.xml")
 	self.xmlDoc:RegisterCallback("OnDocLoaded", self)
@@ -68,6 +66,8 @@ end
 function SpaceStashInventory:OnDocLoaded()
 	if self.xmlDoc ~= nil and self.xmlDoc:IsLoaded() then
 	    self.wndMain = Apollo.LoadForm(self.xmlDoc, "SpaceStashInventoryForm", nil, self)
+	    self.wndDeleteConfirm 	= Apollo.LoadForm(self.xmlDoc, "InventoryDeleteNotice", nil, self)
+
 		if self.wndMain == nil then
 			Apollo.AddAddonErrorText(self, "Could not load the main window for some reason.")
 			return
@@ -75,19 +75,113 @@ function SpaceStashInventory:OnDocLoaded()
 		self.xmlDoc = nil
 
 		self.wndMain:Show(false, true)
-		
-
-		SpaceStashInventory:UpdateConfig(self)
-
+		self.wndDeleteConfirm:Show(false, true)
+		Apollo.RegisterEventHandler("InterfaceMenuListHasLoaded", "OnInterfaceMenuListHasLoaded", self)
 		Apollo.RegisterEventHandler("PlayerCurrencyChanged", "OnPlayerCurrencyChanged", self)
 		Apollo.RegisterEventHandler("WindowMove", "OnWindowMove", self)
-		
-		-- Register handlers for events, slash commands and timer, etc.
+		Apollo.RegisterEventHandler("PlayerEquippedItemChanged", "OnBagsChange", self)
 		Apollo.RegisterEventHandler("KeyDown", "OnKeyDown", self)
+
+		Apollo.RegisterEventHandler("GuildBank_ShowPersonalInventory", "OnVisibilityToggle", self)
+		Apollo.RegisterEventHandler("InterfaceMenu_ToggleInventory", "OnVisibilityToggle", self)
+		Apollo.RegisterEventHandler("ToggleInventory", "OnVisibilityToggle", self)
+		Apollo.RegisterEventHandler("ShowInventory", "OnVisibilityToggle", self)
+		
+		Apollo.RegisterEventHandler("DragDropSysBegin", "OnSystemBeginDragDrop", self)
+		Apollo.RegisterEventHandler("DragDropSysEnd", "OnSystemEndDragDrop", self)
+		Apollo.RegisterEventHandler("SplitItemStack", "OnSplitItemStack", self)
 		Apollo.RegisterSlashCommand("ssi", "OnSSCmd", self)
 		-- Do additional Addon initialization here
-
 	end
+end
+
+function SpaceStashInventory:OnInterfaceMenuListHasLoaded()
+	Event_FireGenericEvent("InterfaceMenuList_NewAddOn", Apollo.GetString("InterfaceMenu_Inventory"), {"InterfaceMenu_ToggleInventory", "Inventory", ""})
+end
+
+-----------------------------------------------------------------------------------------------
+-- Item Deleting (c) Carbine
+-----------------------------------------------------------------------------------------------
+function SpaceStashInventory:OnSystemBeginDragDrop(wndSource, strType, iData)
+	if strType ~= "DDBagItem" then return end
+	Print(inspect.inspect(iData))
+	Sound.Play(Sound.PlayUI45LiftVirtual)
+end
+
+function SpaceStashInventory:OnSystemEndDragDrop(strType, iData)
+	if not self.wndMain or not self.wndMain:IsValid() or not self.wndMain:FindChild("TrashIcon") or strType == "DDGuildBankItem" or strType == "DDWarPartyBankItem" or strType == "DDGuildBankItemSplitStack" then
+		return -- TODO Investigate if there are other types
+	end
+
+	Sound.Play(Sound.PlayUI46PlaceVirtual)
+end
+
+function SpaceStashInventory:OnDeleteCancel()
+	self.wndDeleteConfirm:SetData(nil)
+	self.wndDeleteConfirm:Close()
+end
+
+function SpaceStashInventory:InvokeDeleteConfirmWindow(iData) 
+	local itemData = Item.GetItemFromInventoryLoc(iData)
+	if itemData and not itemData:CanDelete() then
+		return
+	end
+	self.wndDeleteConfirm:SetData(iData)
+	self.wndDeleteConfirm:Show(true)
+	self.wndDeleteConfirm:ToFront()
+	self.wndDeleteConfirm:FindChild("DeleteBtn"):SetActionData(GameLib.CodeEnumConfirmButtonType.DeleteItem, iData)
+	Sound.Play(Sound.PlayUI55ErrorVirtual)
+end
+
+function SpaceStashInventory:OnDeleteConfirm()
+	self:OnDeleteCancel()
+end
+
+function SpaceStashInventory:OnBagDragDropCancel(wndHandler, wndControl, strType, iData, eReason)
+	if strType ~= "DDBagItem" or eReason == Apollo.DragDropCancelReason.EscapeKey or eReason == Apollo.DragDropCancelReason.ClickedOnNothing then
+		return false
+	end
+
+	if eReason == Apollo.DragDropCancelReason.ClickedOnWorld or eReason == Apollo.DragDropCancelReason.DroppedOnNothing then
+		self:InvokeDeleteConfirmWindow(iData)
+	end
+	return false
+end
+
+-- Trash Icon
+function SpaceStashInventory:OnDragDropTrash(wndHandler, wndControl, nX, nY, wndSource, strType, iData)
+	if strType == "DDBagItem" then
+		self:InvokeDeleteConfirmWindow(iData)
+	end
+	return false
+end
+-----------------------------------------------------------------------------------------------
+-- Stack Splitting (c) Carbine
+-----------------------------------------------------------------------------------------------
+
+function SpaceStashInventory:OnSplitItemStack(item)
+	if not item then return end
+	local wndSplit = self.wndMain:FindChild("SplitStackContainer")
+	local nStackCount = item:GetStackCount()
+	if nStackCount < 2 then
+		wndSplit:Show(false)
+		return
+	end
+	wndSplit:SetData(item)
+	wndSplit:FindChild("SplitValue"):SetValue(1)
+	wndSplit:FindChild("SplitValue"):SetMinMax(1, nStackCount - 1)
+	wndSplit:Show(true)
+end
+
+function SpaceStashInventory:OnSplitStackCloseClick()
+	self.wndMain:FindChild("SplitStackContainer"):Show(false)
+end
+
+function SpaceStashInventory:OnSplitStackConfirm(wndHandler, wndCtrl)
+	local wndSplit = self.wndMain:FindChild("SplitStackContainer")
+	local tItem = wndSplit:GetData()
+	wndSplit:Show(false)
+	self.wndMain:FindChild("BagWindow"):StartSplitStack(tItem, wndSplit:FindChild("SplitValue"):GetValue())
 end
 
 -----------------------------------------------------------------------------------------------
@@ -95,39 +189,33 @@ end
 -----------------------------------------------------------------------------------------------
 function SpaceStashInventory:OnSave(eLevel)
 
-	if eLevel ~= GameLib.CodeEnumAddonSaveLevel.Character then 
+	if eLevel ~= 1 then 
 		return 
 	end
-	local tSave = self.config
 
 	return self.tConfig
 end
 
 function SpaceStashInventory:OnRestore(eLevel, tData )
-
-	if not tData or tData.version.MAJOR ~= MAJOR then
+	if eLevel ~= 1 then 
 		return
 	end
-
-	self.tSave = tData
+	
 	self.tConfig = tData
-end
------------------------------------------------------------------------------------------------
--- SpaceStashInventory OnDocLoaded
------------------------------------------------------------------------------------------------
 
+end
 
 -----------------------------------------------------------------------------------------------
 -- Currencies Functions
 -----------------------------------------------------------------------------------------------
 -- currency event fired
 function SpaceStashInventory:OnPlayerCurrencyChanged()
-	if self.wndMain then 
-	 	SpaceStashInventory:UpdateCashAmount(self) 
+	if self.wndMain:IsShown() then 
+	 	self:UpdateCashAmount() 
 	 end
 end
 
-function SpaceStashInventory:UpdateCashAmount(self)
+function SpaceStashInventory:UpdateCashAmount()
 	self.wndMain:FindChild("CashWindow"):SetAmount(GameLib.GetPlayerCurrency(), true)
 	self.wndMain:FindChild("CurrencyWindow"):SetAmount(GameLib.GetPlayerCurrency(self.tConfig.currencies.eCurrencyType):GetAmount())
 end
@@ -136,64 +224,98 @@ end
 -- SpaceStashInventory Commands 
 ---------------------------------------------------------------------------------------------------
 
--- on /ss console command
+-- on /ssi console command
 function SpaceStashInventory:OnSSCmd(strCommand, strParam)
 	if strParam == "" then 
-		SpaceStashInventory:OnVisibilityToggle(self)
+		self:OnVisibilityToggle()
 	elseif strParam == "help" then 
-		ChatSystemLib.PostOnChannel(ChatSystemLib.ChatChannel_Command, "/ssi - toggle bag visibility\n/ssi help - show this help\n/ssi info - show a debug array\n/ssi option currency [ElderGems,Prestige,Renown,CraftingVouchers] - change the tracked currency")
+		ChatSystemLib.PostOnChannel(ChatSystemLib.ChatChannel_Command, 
+				[[/ssi : This command toggle the visibility state of the SpaceStashInventory\n
+				/ssi help : Show this help\n
+				/ssi option RowSize [number] : define the number of item per row you want.\n
+				/ssi option IconSize [number] : define size of item icons.\n
+				/ssi option currency [ElderGems,Prestige,Renown,CraftingVouchers] : define the currently tracked alternative currency.\n
+				/ssi redraw : debuging purpose - redraw the bag window\n
+				/ssi info : debuging purpose - send the metatable to GeminiConsole]]
+			)
+
 	elseif strParam == "info" then 
 		self.glog:info(self)
+	elseif strParam == "redraw" then
+		self:UpdateWindow()
 	elseif string.find(string.lower(strParam), "option") ~= nil then
-		strParam = string.lower(strParam)
+		
 		local args = {}
 
 		for arg in string.gmatch(strParam, "[%a%d]+") do table.insert(args, arg) end
 
 		if args[2] == "currency" then
+
 			local eType = tCurrencies[args[3]]
 			if eType ~= nil then
 				self.tConfig.currencies.eCurrencyType = eType
-				self.wndMain:FindChild("CurrencyWindow"):SetMoneySystem(self.tConfig.currencies.eCurrencyType)
-				SpaceStashInventory:UpdateCashAmount(self)
+				self.wndMain:FindChild("CurrencyWindow"):SetMoneySystem(eType)
+				self:UpdateCashAmount()
 			else
 				ChatSystemLib.PostOnChannel(ChatSystemLib.ChatChannel_Command, args[3] .. " is not a valid currency[ElderGems,Prestige,Renown,CraftingVouchers]")
 			end
-		elseif args[2] == "squaresize" then
+		elseif string.lower(args[2]) == "rowsize" then
 			local size = string.match(args[3],"%d+")
 			if size ~= nil then
-				self.tConfig.window.SquareSize = size
-				SpaceStashInventory:UpdateWindowConfig(self)
-				SpaceStashInventory:UpdateConfig(self)
+				self.tConfig.window.RowSize = size
+				self:UpdateWindow()
 			end
-		elseif args[2] == "boxperrow" then
+		elseif string.lower(args[2]) == "iconsize" then
 			local size = string.match(args[3],"%d+")
 			if size ~= nil then
-				self.tConfig.window.BoxPerRow = size
-				SpaceStashInventory:UpdateWindowConfig(self)
-				SpaceStashInventory:UpdateConfig(self)
+				self.tConfig.window.IconSize = size
+				self:UpdateWindow()
 			end
 		end
 	end
 end
 ---------------------------------------------------------------------------------------------------
--- SpaceStashInventoryForm Visibility 
+-- SpaceStashInventoryForm 
 ---------------------------------------------------------------------------------------------------
+-- Reacting to the inventory keybind
+-- TODO
 function SpaceStashInventory:OnKeyDown(wndHandler, wndControl, strKeyName, nCode, eModifier)
 
 end
 
-function  SpaceStashInventory:OnWindowMove(  )
+-- update the windows position in the config as the user move it to save position between sessions.
+function  SpaceStashInventory:OnWindowMove()
+	-- TODO: Check that the window is in the screen
+	-- TODO: add an option to keep the entire frame in screen
 	self.tConfig.window.location = self.wndMain:GetLocation():ToTable()
 end
 
+-- When the SalvageButton is pressed.
 function SpaceStashInventory:OnSalvageButton()
+	-- TODO: option to configure how button work
+	-- TODO: option to set the fired event.
+	-- MODDERS : if you have a personal addon for salvaging, just make it to handle "RequestSalvageAll". You will need to disable the current ImprovedSalvage addon packed with SpaceStash.
 	Event_FireGenericEvent("RequestSalvageAll", tAnchors)
 end
+
+function SpaceStashInventory:OnTradskillStashButton()
+	Event_FireGenericEvent("ToggleTradeskillInventoryFromBag", tAnchors)
+end
+
 -- calculation of the windows offets
-function SpaceStashInventory:UpdateWindowConfig(self)
-	self.tConfig.window.location.nOffsets[3] = self.tConfig.window.location.nOffsets[1] + self.tConfig.window.SquareSize * self.tConfig.window.BoxPerRow + 12
-	
+function SpaceStashInventory:UpdateConfig()
+
+	self.tConfig.window.location.nOffsets[3] = self.tConfig.window.location.nOffsets[1] + self.tConfig.window.IconSize * self.tConfig.window.RowSize + 12
+
+	local rowCount = math.floor(self.nInventorySize / self.tConfig.window.RowSize)
+	if self.nInventorySize % self.tConfig.window.RowSize ~= 0 then rowCount = rowCount +1 end
+
+	self.tConfig.window.location.nOffsets[4] = self.tConfig.window.location.nOffsets[2] + rowCount * self.tConfig.window.IconSize + 24 + 54
+
+end
+
+-- return the size of the inventory and update the window and config if doUpdate is set
+function SpaceStashInventory:OnBagsChange()
 	local nInventorySize = 16
 	local itemEquipped = GameLib.GetPlayerUnit():GetEquippedItems()
 
@@ -202,51 +324,52 @@ function SpaceStashInventory:UpdateWindowConfig(self)
 			nInventorySize = nInventorySize + itemEquipped[idx]:GetBagSlots()
 		end
 	end
-
-	local rowCount = math.floor(nInventorySize / self.tConfig.window.BoxPerRow)
-	if nInventorySize % self.tConfig.window.BoxPerRow ~= 0 then rowCount = rowCount +1 end
-	self.glog:info(rowCount)
-	self.tConfig.window.location.nOffsets[4] = self.tConfig.window.location.nOffsets[2] + rowCount * self.tConfig.window.SquareSize + 24 + 54
+	
+	self.nInventorySize = nInventorySize
+	
+	self:UpdateWindow()
 end
-
 
 -- when the Cancel button is clicked
 function SpaceStashInventory:OnClose()
-	SpaceStashInventory:OnVisibilityToggle(self)
+	self:OnVisibilityToggle(self)
 end
 
-function SpaceStashInventory:OnVisibilityToggle(self)
+function SpaceStashInventory:OnVisibilityToggle()
 	if self.wndMain:IsShown() then
 		self.wndMain:Show(false)
+		self.wndMain:FindChild("BagWindow"):MarkAllItemsAsSeen()
 		Sound.Play(Sound.PlayUIBagClose)
 	else
-		SpaceStashInventory:UpdateCashAmount(self)
+		self:OnBagsChange()
 		self.wndMain:Show(true)
-
 		Sound.Play(Sound.PlayUIBagOpen)
 	end
 end
 
-function SpaceStashInventory:UpdateConfig(self)
-	SpaceStashInventory:UpdateWindowConfig(self)
+-- Update the window sizing an properties (not the 'volatiles' as currencies amounts, new item icon, etc.)
+function SpaceStashInventory:UpdateWindow()
+	self:UpdateConfig()
+
 	self.wndMain:SetAnchorOffsets(self.tConfig.window.location.nOffsets[1],self.tConfig.window.location.nOffsets[2],self.tConfig.window.location.nOffsets[3],self.tConfig.window.location.nOffsets[4])
-	self.wndMain:FindChild("BagWindow"):SetSquareSize(self.tConfig.window.SquareSize, self.tConfig.window.SquareSize) 
-	self.wndMain:FindChild("BagWindow"):SetBoxesPerRow(self.tConfig.window.BoxPerRow)
+	self.wndMain:FindChild("BagWindow"):SetSquareSize(self.tConfig.window.IconSize, self.tConfig.window.IconSize) 
+	self.wndMain:FindChild("BagWindow"):SetBoxesPerRow(self.tConfig.window.RowSize)
 	self.wndMain:FindChild("CurrencyWindow"):SetMoneySystem(self.tConfig.currencies.eCurrencyType)
-	SpaceStashInventory:UpdateCashAmount(self)
+	self:UpdateCashAmount()
 end
 
-
-
+-- Generate the tooltips. From stock addon
+-- TODO: Mark item as viewed
 function SpaceStashInventory:OnGenerateTooltip(wndControl, wndHandler, tType, item)
 	if wndControl ~= wndHandler then return end
 	wndControl:SetTooltipDoc(nil)
 	if item ~= nil then
 		local itemEquipped = item:GetEquippedItemForItemType()
+
 		Tooltip.GetItemTooltipForm(self, wndControl, item, {bPrimary = true, bSelling = false, itemCompare = itemEquipped})
-		-- Tooltip.GetItemTooltipForm(self, wndControl, itemEquipped, {bPrimary = false, bSelling = false, itemCompare = item})
 	end
 end
+
 -----------------------------------------------------------------------------------------------
 -- SpaceStashInventory Instance
 -----------------------------------------------------------------------------------------------
@@ -259,8 +382,6 @@ end
 
 Apollo.RegisterAddon(SpaceStashInventory:new(), false, "", {
 	"Gemini:Logging-1.2", 
-	"Drafto:Lib:inspect-1.2",
-	"GeminiConsole",
-	"ImprovedSalvage"
+	"Drafto:Lib:inspect-1.2"
 })
  
